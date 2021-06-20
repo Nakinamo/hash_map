69c016e030b2940392d6c250b19033120df2174c
nakinamo <kossanikolay@gmail.com>
2021-06-20 13:49:36 +0300

    added commets

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -2,52 +2,65 @@
 #include <utility>
 #include <vector>
 
+// Hash table iterator.
 template<class ValueType>
 class HashMapIterator {
     typename std::vector<std::vector<ValueType>>* Table;
-    size_t first, second;
-public:
+    size_t First, Second;
+  public:
+    // Default constructor.
     HashMapIterator() {}
+    // Constructor by table, bucket number and item number.
     HashMapIterator(typename std::vector<std::vector<ValueType>>* t, size_t f, size_t s) {
-        first = f;
-        second = s;
+        First = f;
+        Second = s;
         Table = t;
     }
+    // Returns true if iterator is equal to Oth in O(1) time.
     bool operator==(const HashMapIterator &Oth) const {
-        return first == Oth.first && second == Oth.second && Table == Oth.Table;
+        return First == Oth.first && Second == Oth.second && Table == Oth.Table;
     }
+    // Returns true if iterator is not equal to Oth in O(1) time.
     bool operator!=(const HashMapIterator &Oth) const {
         return !(*this == Oth);
     }
+    // Returns bucket number in O(1) time.
     size_t GetFirst() {
-        return first;
+        return First;
     }
+    // Returns item number in bucket in O(1) time.
     size_t GetSecond() {
-        return second;
+        return Second;
     }
+    // Returns item object in O(1) time.
     ValueType& operator*() {
-        return (*Table)[first][second];
+        return (*Table)[First][Second];
     }
+    // Returns constant item object in O(1) time.
     ValueType operator*() const {
-        return (*Table)[first][second];
+        return (*Table)[First][Second];
     }
+    // Returns item reference in O(1) time.
     ValueType* operator->() {
-        return &(*Table)[first][second];
+        return &(*Table)[First][Second];
     }
+    // Returns constant item reference in O(1) time.
     ValueType const* operator->() const {
-        return &(*Table)[first][second];
+        return &(*Table)[First][Second];
     }
+    // Moves iterator to next item and returns it in O(1) expected time.
     HashMapIterator operator++() {
-        ++second;
-        if (second >= (*Table)[first].size()) {
-            ++first;
-            while (first != Table->size() && (*Table)[first].size() == 0) {
-                ++first;
+        ++Second;
+        if (Second >= (*Table)[First].size()) {
+            ++First;
+            while (First != Table->size() && (*Table)[First].size() == 0) {
+                ++First;
             }
-            second = 0;
+            Second = 0;
         }
         return *this;
     }
+    // Moves iterator to next item and returns its previous state in O(1) expected time.
     HashMapIterator operator++(int) {
         HashMapIterator Old = *this;
         operator++();
@@ -55,46 +68,57 @@ public:
     }
 };
 
+// Constant hash table iterator.
 template<class ValueType>
 class HashMapConstIterator {
     const std::vector<std::vector<ValueType>>* Table;
-    size_t first, second;
-public:
+    size_t First, Second;
+  public:
+    // Default constructor.
     HashMapConstIterator() {}
+    // Constructor by table, bucket number and item number.
     HashMapConstIterator(const std::vector<std::vector<ValueType>>* t, size_t f, size_t s) {
-        first = f;
-        second = s;
+        First = f;
+        Second = s;
         Table = t;
     }
+    // Returns true if constant iterator is equal to Oth in O(1) time.
     bool operator==(const HashMapConstIterator &Oth) const {
-        return first == Oth.first && second == Oth.second && Table == Oth.Table;
+        return First == Oth.first && Second == Oth.second && Table == Oth.Table;
     }
+    // Returns true if constant iterator is not equal to Oth in O(1) time.
     bool operator!=(const HashMapConstIterator &Oth) const {
         return !(*this == Oth);
     }
+    // Returns bucket number in O(1) time.
     size_t GetFirst() {
-        return first;
+        return First;
     }
+    // Returns item number in bucket in O(1) time.
     size_t GetSecond() {
-        return second;
+        return Second;
     }
+    // Returns constant item object in O(1) time.
     ValueType operator*() const {
-        return (*Table)[first][second];
+        return (*Table)[First][Second];
     }
+    // Returns constant item reference in O(1) time.
     ValueType const* operator->() const {
-        return &(*Table)[first][second];
+        return &(*Table)[First][Second];
     }
+    // Moves constant iterator to next item and returns it in O(1) expected time.
     HashMapConstIterator operator++() {
-        ++second;
-        if (second >= (*Table)[first].size()) {
-            ++first;
-            while (first != Table->size() && (*Table)[first].size() == 0) {
-                ++first;
+        ++Second;
+        if (Second >= (*Table)[First].size()) {
+            ++First;
+            while (First != Table->size() && (*Table)[First].size() == 0) {
+                ++First;
             }
-            second = 0;
+            Second = 0;
         }
         return *this;
     }
+    // Moves constant iterator to next item and returns its previous state in O(1) expected time.
     HashMapConstIterator operator++(int) {
         HashMapConstIterator Old = *this;
         operator++();
@@ -102,19 +126,22 @@ public:
     }
 };
 
+// Bucket hash table with load factor = 10 and resize factor = 2.
 template<class KeyType, class ValueType, class Hash = std::hash<KeyType>>
 class HashMap {
-    static const size_t LoadFactor = 10;
-    static const size_t ResizeFactor = 2;
     Hash HashFunction;
     std::vector<std::vector<std::pair<const KeyType, ValueType>>> Table;
     size_t Sz = 0;
-public:
+  public:
+    static constexpr size_t LoadFactor = 10;
+    static constexpr size_t ResizeFactor = 2;
     typedef HashMapIterator<std::pair<const KeyType, ValueType>> iterator;
     typedef HashMapConstIterator<std::pair<const KeyType, ValueType>> const_iterator;
+    // Default constructor.
     HashMap() {
         initialize();
     }
+    // Constructor for begin and end iterators.
     template<class Iterator>
     HashMap(Iterator first, Iterator last) {
         initialize();
@@ -122,15 +149,18 @@ public:
             insert(*i);
         }
     }
+    // Constructor for initializer list.
     HashMap(std::initializer_list<std::pair<KeyType, ValueType>> List) {
         initialize();
         for (auto i = List.begin(); i != List.end(); ++i) {
             insert(*i);
         }
     }
+    // Default constructor with custom hash function.
     HashMap(const Hash& HashFunction): HashFunction(HashFunction) {
         initialize();
     }
+    // Constructor for begin and end iterators with custom hash function.
     template<class Iterator>
     HashMap(Iterator first, Iterator last, const Hash& HashFunction): HashFunction(HashFunction) {
         initialize();
@@ -138,33 +168,40 @@ public:
             insert(*i);
         }
     }
+    // Constructor for initializer list with custom hash function.
     HashMap(std::initializer_list<std::pair<KeyType, ValueType>> List, const Hash& HashFunction): HashFunction(HashFunction) {
         initialize();
         for (auto i = List.begin(); i != List.end(); ++i) {
             insert(*i);
         }
     }
+    // Returns begin iterator in O(1) time.
     iterator begin() {
         if (empty()) {
             return end();
         }
         return GetIterator();
     }
+    // Returns end iterator in O(1) time.
     iterator end() {
         return GetIterator(Table.size());
     }
+    // Returns constanst begin iterator in O(1) time.
     const_iterator begin() const {
         if (empty()) {
             return end();
         }
         return GetIterator();
     }
+    // Returns constanst end iterator in O(1) time.
     const_iterator end() const {
         return GetIterator(Table.size());
     }
+    // Returns hash function in O(1) time.
     Hash hash_function() const {
         return HashFunction;
     }
+    // Inserts item in O(1) amortized time. May invalidate iterators.
     void insert(const std::pair<KeyType, ValueType>& Item) {
         if (find(Item.first) != end()) {
             return;
@@ -172,10 +209,12 @@ public:
         rebuild();
         add(Item);
     }
+    // Erases item in O(1) amortized time. May invalidate iterators.
     void erase(const KeyType& Key) {
         rebuild();
         del(Key);
     }
+    // Returns iterator of item by key in O(bucket size). If hash function is good, works in O(1) expected time.
     iterator find(const KeyType& Key) {
         if (empty()) {
             return end();
@@ -188,6 +227,7 @@ public:
         }
         return end();
     }
+    // Returns constant iterator of item by key in O(bucket size). If hash function is good, works in O(1) expected time.
     const_iterator find(const KeyType& Key) const {
         if (empty()) {
             return end();
@@ -200,11 +240,13 @@ public:
         }
         return end();
     }
+    // Clears table in O(size) time.
     void clear() {
         Sz = 0;
         Table.clear();
         initialize();
     }
+    // Assignment operator. Works in O(Oth.size) time.
     HashMap operator=(const HashMap& Oth) {
         clear();
         for (auto elem : Oth) {
@@ -212,6 +254,7 @@ public:
         }
         return *this;
     }
+    // Returns item by key in O(1) time. If no items stored by this key, creates a new item.
     ValueType& operator[](const KeyType& Key) {
         auto j = find(Key);
         if (j == end()) {
@@ -220,6 +263,7 @@ public:
         }
         return j->second;
     }
+    // Returns item by key in O(1) time. If no items stored by this key, throws out of range exception. If an exception is thrown, there are no changes in the container.
     const ValueType& at(const KeyType& Key) const {
         auto j = find(Key);
         if (j == end()) {
@@ -227,31 +271,39 @@ public:
         }
         return j->second;
     }
+    // Returns number of stored items in O(1) time.
     size_t size() const {
         return Sz;
     }
+    // Returns true if the container does not store any item in O(1) time.
     bool empty() const {
         return Sz == 0;
     }
 
-private:
+  private:
+    // Resizes table to one bucket.
     void initialize() {
         Table.resize(1);
     }
+    // Returns hash function of a key in O(1) time.
     size_t GetHash(const KeyType& Key) const {
         return HashFunction(Key) % Table.size();
     }
+    // Returns iterator for bucket of number f and item of number s in the bucket in O(1) time.
     iterator GetIterator(size_t f = 0, size_t s = 0) {
         return iterator(&Table, f, s);
     }
+    // Returns constant iterator for bucket of number f and item of number s in the bucket in O(1) time.
     const_iterator GetIterator(size_t f = 0, size_t s = 0) const {
         return const_iterator(&Table, f, s);
     }
+    // Inserts item in bucket of its hash in O(1) time.
     void add(const std::pair<KeyType, ValueType>& Item) {
         ++Sz;
         size_t i = GetHash(Item.first);
         Table[i].push_back(Item);
     }
+    // Erases item from bucket of its hash in O(bucket size) time. If hash function is good, works in expected O(1) time.
     void del(const KeyType& Key) {
         size_t i = GetHash(Key);
         std::vector<std::pair<const KeyType, ValueType>> NewBucket;
@@ -265,6 +317,8 @@ private:
             --Sz;
         }
     }
+    // Rebuilds table with 'resize factor' times more buckets if number of items stored is more than product of load factor and buckets number in O(size) time.
+    // Rebuilds table with 'resize factor' times less buckets if product of number of items stored and load factor is less than buckets number in O(size) time.
     void rebuild() {
         if (size() >= Table.size() * LoadFactor) {
             Sz = 0;
